fba3594ebc3ebecba0508ce72b666e0798115a88
Karl Lapitan <karllapitan5@gmail.com>
2025-11-04 00:31:02 +0800

    fix the attribution on expenseChart

--- React-Tailwind-MiniProjects/ExpenseChart/src/Components/attribution.jsx
+++ React-Tailwind-MiniProjects/ExpenseChart/src/Components/attribution.jsx
@@ -3,8 +3,8 @@ const Attribution = () => {
 
 return (
  <div className="w-sm ">
-    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-Red500">Frontend Mentor</a>. 
-    Coded by <a href="https://github.com/KLapitan" className="text-Red500">Karl Lapitan</a>.
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-">Frontend Mentor</a>. 
+    Coded by <a href="#" className="text-Red500">Karl Lapitan</a>.
   </div>
 
 )
