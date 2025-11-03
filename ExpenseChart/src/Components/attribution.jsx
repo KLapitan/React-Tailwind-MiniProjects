36b823219299ab17922095a36a420961c21c2ec9
Karl Lapitan <karllapitan5@gmail.com>
2025-11-04 00:32:16 +0800

    Fix expense chart

--- ExpenseChart/src/Components/attribution.jsx
+++ ExpenseChart/src/Components/attribution.jsx
@@ -2,7 +2,7 @@ const Attribution = () => {
 
 
 return (
- <div className="w-[290px]  border  flex flex-row">
+ <div className="w-[290px]   flex flex-row">
    <p className=" text-xs  w-auto"> Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-Red500">Frontend Mentor</a>. </p> 
    <p className=" text-xs  w-auto">Coded by <a href="https://github.com/KLapitan" className="text-Red500">Karl Lapitan</a>.
 </p>   </div>
