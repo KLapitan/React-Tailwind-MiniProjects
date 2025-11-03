e51cec9f1d38d356260e744a2e512b378ed5c2d9
Karl Lapitan <karllapitan5@gmail.com>
2025-11-03 23:47:07 +0800

    Added:ExpenseChart to Projects

---
+++ ExpenseChart/src/Components/attribution.jsx
@@ -0,0 +1,13 @@
+const Attribution = () => {
+
+
+return (
+ <div className="w-sm ">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-Red500">Frontend Mentor</a>. 
+    Coded by <a href="https://github.com/KLapitan" className="text-Red500">Karl Lapitan</a>.
+  </div>
+
+)
+}
+
+export default Attribution
